32245489923280a2d4d73a6720e854ddaa90b933
James Kearney <james.kearney@snyk.io>
2024-02-15 10:29:58 -0600

    feat: add preflight for node version

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,14 @@
 
 import * as yargs from 'yargs';
 
+var node_ver_str = process.versions.node.split('.')[0];
+var node_ver: number = +node_ver_str;
+
+if (node_ver < 14) {
+	console.log("Detected Node.js v" + node_ver + " -  v14 or later is required.");
+	process.exit(1);
+}
+
 yargs
   .commandDir('cmds')
   .help()
